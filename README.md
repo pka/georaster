37718a7b140451824e506e48b28cdd3403d05009
Pirmin Kalberer <pka@sourcepole.ch>
2022-12-16 20:54:01 +0100

    Make crop example generic

--- README.md
+++ README.md
@@ -4,6 +4,22 @@ georaster
 Rust library for accessing geospatial raster images.
 
 
+## Usage examples
+
+Extract part of GeoTIFF into a PNG:
+```rust
+let img_file = BufReader::new(File::open("N265E425.tif").unwrap());
+let mut tiff = GeoTiffReader::open(img_file).unwrap();
+let (x0, y0, w, h) = (2500, 3000, 100, 100);
+let mut img = ImageBuffer::new(w, h);
+for (x, y, pixel) in tiff.pixels(x0, y0, w, h) {
+    if let RasterValue::U16(v) = pixel {
+        img.put_pixel(x - x0, y - y0, image::Luma([v]));
+    }
+}
+img.save("dtm.png").unwrap();
+```
+
 ## Running the examples
 
 Download test data:
@@ -14,7 +30,5 @@ make
 ```
 
 ```
-cargo run --example geotiff_dtm
-cargo run --example cog
-cargo run --example extract_png
+cargo run --example crop data/tiff/N265E425.tif 100x100+2500+3000 dtm.png
 ```
---
+++ examples/crop.rs
@@ -0,0 +1,32 @@
+use georaster::geotiff::{GeoTiffReader, RasterValue};
+use image::ImageBuffer;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::str::FromStr;
+
+fn main() {
+    let src_fn = env::args().nth(1).expect("Input file required");
+    let window = env::args().nth(2).expect("Window size required");
+    let dst_fn = env::args().nth(3).expect("Output file required");
+
+    // window size example: 100x100+2500+3000
+    let parts: Vec<_> = window
+        .split(&['x', '+'])
+        .map(|s| u32::from_str(s).expect("Invalid number"))
+        .collect();
+    let w = *parts.get(0).expect("witdh missing");
+    let h = *parts.get(1).expect("height missing");
+    let x0 = *parts.get(2).unwrap_or(&0);
+    let y0 = *parts.get(3).unwrap_or(&0);
+
+    let img_file = BufReader::new(File::open(src_fn).expect("Cannot open input file"));
+    let mut tiff = GeoTiffReader::open(img_file).expect("Cannot create decoder");
+    let mut img = ImageBuffer::new(w, h);
+    for (x, y, pixel) in tiff.pixels(x0, y0, w, h) {
+        if let RasterValue::U16(v) = pixel {
+            img.put_pixel(x - x0, y - y0, image::Luma([v]));
+        }
+    }
+    img.save(dst_fn).unwrap();
+}
