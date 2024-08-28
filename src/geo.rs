2b26710efdef976d6a5733bb758237444156b458
Michael Behrens <mfbehrens99@gmail.com>
2024-08-29 00:18:55 +0200

    Add documentation

--- src/geo.rs
+++ src/geo.rs
@@ -1,8 +1,46 @@
 //! Common geo types
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+
+/// 2D coordinate representation
+///
+/// Coordinates can be converted from and to `geo` by activating
+/// the `geo-crate` feature and `geodesy` by the `geodesy-crate` feature.
+///
+/// ```rust
+/// use georaster::Coordinate;
+///
+/// let lat = 50.013;
+/// let lon = 160.423;
+/// let coordinate1 = Coordinate::new(lat, lon);
+/// let coordinate2 = Coordinate { x: lon, y: lat };
+/// assert_eq!(coordinate1.x, coordinate2.x);
+/// assert_eq!(coordinate1.y, coordinate2.y);
+/// ```
+#[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
 pub struct Coordinate {
+    /// Longitude of the `Coordiate` type
     pub x: f64,
+    /// Latitude of the `Coordiate` type
     pub y: f64,
+}
+
+impl Coordinate {
+    /// Create a new coordinate from x and y
+    ///
+    /// ```rust
+    /// use georaster::Coordinate;
+    ///
+    /// let lat = 50.013;
+    /// let lon = 160.423;
+    /// let coordinate = Coordinate::new(lat, lon);
+    /// assert_eq!(coordinate.x, lon);
+    /// assert_eq!(coordinate.y, lat);
+    /// ```
+    pub fn new(latitude: f64, longitude: f64) -> Self {
+        Self {
+            x: longitude,
+            y: latitude,
+        }
+    }
 }
 
 impl From<(f64, f64)> for Coordinate {
