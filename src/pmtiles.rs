e9e09c06ad906e3acf61cb5536f2372f784f04a9
Pirmin Kalberer <pirmin.kalberer@gmail.com>
2025-08-09 16:37:08 +0200

    Decode tile image and read pixel

--- src/pmtiles.rs
+++ src/pmtiles.rs
@@ -1,5 +1,7 @@
-use bytes::Bytes;
+use crate::geotiff::RasterValue;
+use image::{DynamicImage, GenericImageView, ImageReader, Pixel};
 use pmtiles::{AsyncPmTilesReader, PmtError, PmtResult, TileCoord};
+use std::io::Cursor;
 
 /// PMTiles raster reader
 pub struct PmtilesRasterReader {
@@ -13,9 +15,32 @@ impl PmtilesRasterReader {
         Ok(Self { reader })
     }
 
-    pub async fn get_tile(&self, z: u8, x: u32, y: u32) -> PmtResult<Option<Bytes>> {
+    pub async fn get_tile(&self, z: u8, x: u32, y: u32) -> PmtResult<DynamicImage> {
         let coord = TileCoord::new(z, x, y).ok_or(PmtError::InvalidEntry)?;
-        self.reader.get_tile(coord).await
+        let bytes = self
+            .reader
+            .get_tile(coord)
+            .await?
+            .ok_or(PmtError::InvalidEntry)?;
+
+        let img = ImageReader::new(Cursor::new(bytes))
+            .with_guessed_format()?
+            .decode()
+            .unwrap();
+        Ok(img)
+    }
+
+    pub async fn get_pixel(
+        &self,
+        z: u8,
+        x: u32,
+        y: u32,
+        px: u32,
+        py: u32,
+    ) -> PmtResult<RasterValue> {
+        let tile = self.get_tile(z, x, y).await?;
+        let [r, g, b, a] = tile.get_pixel(px, py).to_rgba().0;
+        Ok(RasterValue::Rgba8(r, g, b, a))
     }
 }
 
@@ -23,21 +48,26 @@ impl PmtilesRasterReader {
 mod tests {
     use super::*;
 
+    async fn test_tiles() -> PmtilesRasterReader {
+        PmtilesRasterReader::open("data/ch-nw.pmtiles")
+            .await
+            .unwrap()
+    }
+
     #[tokio::test]
     async fn test_tile() {
-        let pmtiles = PmtilesRasterReader::open("data/ch-nw.pmtiles")
-            .await
-            .unwrap();
-        // Chasseral 47.133037, 7.059309 1607m
+        let pmtiles = test_tiles().await;
         assert_eq!(
-            pmtiles
-                .get_tile(12, 2128, 1438)
-                .await
-                .unwrap()
-                .unwrap()
-                .len(),
-            316992
+            pmtiles.get_pixel(12, 2128, 1438, 10, 10).await.unwrap(),
+            RasterValue::Rgba8(131, 4, 183, 255)
         );
         //assert_eq!(get_tile(10, 532, 359).await.unwrap().len(), 316992);
+    }
+
+    #[tokio::test]
+    async fn test_pixel() {
+        let pmtiles = test_tiles().await;
+        // Chasseral 47.133037, 7.059309 1607m
+        assert_eq!(pmtiles.get_tile(12, 2128, 1438).await.unwrap().width(), 512);
     }
 }
